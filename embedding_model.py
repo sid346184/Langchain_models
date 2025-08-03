fe2ee4a6397ee014f91dff53bb59502f03e88b25
Siddhant Gupta <gsid306@gmail.com>
2025-08-03 23:00:40 +0530

    added Embedding Models

--- embedding_model.py
+++
@@ -1,8 +0,0 @@
-from langchain_groq import ChatGroq
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from dotenv import load_dotenv
-load_dotenv()
-embeddings= GoogleGenerativeAIEmbeddings(model='models/gemini-embedding-001')
-vector=embeddings.embed_query("HI")
-
-print(vector[:5])
